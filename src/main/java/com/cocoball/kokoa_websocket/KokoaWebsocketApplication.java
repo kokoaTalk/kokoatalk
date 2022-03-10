650eb4f59f3d54408fef4cb97f39a2fa783c09b5
hyena0608 <goatsfish@gmail.com>
2022-03-10 23:30:01 +0900

    init

---
+++ src/main/java/com/cocoball/kokoa_websocket/KokoaWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.cocoball.kokoa_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokoaWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KokoaWebsocketApplication.class, args);
+    }
+
+}
